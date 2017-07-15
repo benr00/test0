2bb7bb00d881acdc0551d2d93b9d93e220e18409
benr00 <359104038@qq.com>
2017-07-15 22:08:40 +0800

    index.htmlchange

---
+++ js/demo1.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: iceStone
+ * @Date:   2016-01-25 18:52:52
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2017-07-15 21:36:53
+ */
+
+'use strict';
+
+
+console.log(111);
+(function(){
+
+	console.log(222)
+
+})(window);
